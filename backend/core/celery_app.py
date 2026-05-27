8f969aae8a2080a3931ea5bf2652b2cd68be97c0
blackeagle686 <mathematecs1@gmail.com>
2026-05-27 23:47:58 +0300

    Updated Redis URL in celery_app.py

--- backend/core/celery_app.py
+++ backend/core/celery_app.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
-REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6385/0")
 
 celery_app = Celery(
     "phoenix_tasks",
