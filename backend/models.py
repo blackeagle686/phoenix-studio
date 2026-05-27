43148377452b089ca3688d4223b2111853d71097
blackeagle686 <mathematecs1@gmail.com>
2026-05-27 22:43:59 +0300

    Added user fields: first_name, last_name, location, phone_number, email

--- backend/models.py
+++ backend/models.py
@@ -9,6 +9,11 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+    first_name = Column(String, nullable=True)
+    last_name = Column(String, nullable=True)
+    location = Column(String, nullable=True)
+    phone_number = Column(String, nullable=True)
+    email = Column(String, unique=True, index=True, nullable=True)
 
     workspaces = relationship("Workspace", back_populates="owner")
 
