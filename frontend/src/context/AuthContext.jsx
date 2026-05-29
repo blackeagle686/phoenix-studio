9851b7a3779649aa311857b6c954015378f5f6a1
blackeagle686 <mathematecs1@gmail.com>
2026-05-29 16:23:42 +0300

    new updates at: 2026-05-29 16:23:42

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,7 +1,9 @@
 import React, { createContext, useState, useEffect } from 'react';
 
 export const AuthContext = createContext();
-export const baseurl = "http://localhost:8010/api"
+export const baseurl = window.location.origin === "http://localhost:5173"
+  ? "http://localhost:8010/api"
+  : window.location.origin + "/api";
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem('token'));
